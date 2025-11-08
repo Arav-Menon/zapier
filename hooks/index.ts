2960b5127e14c5c1433c5f92fbe87f97e670e806
Arav menon <aravmenon.ak@gmail.com>
2025-11-08 10:12:13 +0530

    implemnting hooks

---
+++ hooks/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
+
+  
+
+});
