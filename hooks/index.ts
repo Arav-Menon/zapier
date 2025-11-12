6fc59fba5234d822807668a965e530d1c62cdd23
Arav menon <aravmenon.ak@gmail.com>
2025-11-13 00:21:55 +0530

    setup zaps entry

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,14 +1,31 @@
 import express from "express";
+import { hooks_db } from "@hooks_db/index";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
   const body = req.body;
 
-  
+  await hooks_db.$transaction(async (tx) => {
+    const rn = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body,
+      },
+    });
 
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: rn.id,
+      },
+    });
+  });
+
+  res.status(201).json({
+    message: "Webhook recieved",
+  });
 });
